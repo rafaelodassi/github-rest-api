ed472dfd9e0be798278534681f7566fc8557ceba
rafaelodassi <rafaelodassi@hotmail.com>
2019-09-12 00:06:49 -0300

    Dev header com busca

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import logo from '../../assets/img/logo.png';
+import { ReactComponent as IconSearch } from '../../assets/icons/search.svg';
+import { ReactComponent as IconClose } from '../../assets/icons/close.svg';
 
 import './header.scss';
 
 const Header = () => {
+	const [valueSearch, setValueSearch] = useState("");
+
+	const changeValueSearch = (e) => {
+		setValueSearch(e.target.value || "");
+	}
+
+	const clearValueSearch = () => {
+		setValueSearch("");
+	}
+
 	return (
 		<header className="header">
-			header
+			<img title="eSapiens" alt="eSapiens" src={logo} />
+			<div className="container-search-input">
+				<input type="text" placeholder="Pesquisar usuários do GitHub" autoFocus value={valueSearch} onChange={changeValueSearch} />
+
+				<IconSearch className="icon-search" />
+
+				{valueSearch &&
+					<IconClose className="icon-close" onClick={clearValueSearch} />
+				}
+			</div>
+			aas
 		</header>
 	);
 }
