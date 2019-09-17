b21002dd9f90df8a95256dac47adb9a6ad0bff74
rafaelodassi <rafaelodassi@hotmail.com>
2019-09-17 00:28:01 -0300

    Organizando pastas

--- src/store/ducks/repo.js
+++ src/store/ducks/repo.js
@@ -1,46 +1,15 @@
 import { createActions, createReducer } from 'reduxsauce';
 
 export const { Types, Creators } = createActions({
-	apiGetReposByLogin: ['login'],
-	successGetReposByLogin: [],
-	errorGetReposByLogin: [],
-
 	changeOrderReposByStars: [],
 	toggleDrawerRepo: ['repo'],
 	resetOrderReposByStar: []
 });
 
 const initialState = {
-	repos: null,
-	loading: false,
-	error: false,
 	orderReposByStars: 'DESC',
 	drawerRepoDetails: {}
 };
-
-/*GET REPOS BY LOGIN*/
-const apiGetReposByLogin = (state = initialState, action) => {
-	return {
-		...state,
-		repos: null,
-		loading: true,
-		error: false
-	}
-};
-
-const successGetReposByLogin = (state = initialState, action) => ({
-	...state,
-	repos: action.response,
-	loading: false,
-	error: false
-});
-
-const errorGetReposByLogin = (state = initialState, action) => ({
-	...state,
-	repos: null,
-	loading: false,
-	error: action.err
-});
 
 /*CHANGE ORDER REPOS BY STARS*/
 const changeOrderReposByStars = (state = initialState, action) => ({
@@ -61,9 +30,6 @@ const resetOrderReposByStar = (state = initialState, action) => ({
 });
 
 export default createReducer(initialState, {
-	[Types.API_GET_REPOS_BY_LOGIN]: apiGetReposByLogin,
-	[Types.SUCCESS_GET_REPOS_BY_LOGIN]: successGetReposByLogin,
-	[Types.ERROR_GET_REPOS_BY_LOGIN]: errorGetReposByLogin,
 	[Types.CHANGE_ORDER_REPOS_BY_STARS]: changeOrderReposByStars,
 	[Types.TOGGLE_DRAWER_REPO]: toggleDrawerRepo,
 	[Types.RESET_ORDER_REPOS_BY_STAR]: resetOrderReposByStar
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,9 @@
 import { all, fork } from "redux-saga/effects";
 
 import * as userSagas from "./user";
-import * as repoSagas from "./repo";
 
 export default function* rootSaga() {
     yield all([
 		...Object.values(userSagas),
-		...Object.values(repoSagas)
     ].map(fork));
 }
